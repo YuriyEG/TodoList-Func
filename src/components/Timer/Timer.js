2991e17467dcd42dba3e1710873a4b2964c31c59
YuriyEG <you@example.com>
2023-09-20 20:34:36 +0300

    timer is edit

--- src/components/Timer/Timer.js
+++ src/components/Timer/Timer.js
@@ -1,11 +1,19 @@
+/* eslint-disable */
+
 import React, { useState } from 'react';
 
+import './Timer.css';
 import formatSeconds from '../../modules/formatSeconds';
 
-const Timer = (itemId) => {
+const Timer = () => {
   const [count, setCount] = useState(0);
   const [timerId, setTimerId] = useState(false);
+  const [full, setFull] = useState(0);
+  const [isStarted, setIsStarted] = useState(false);
+
   const [pause, setPause] = useState(false);
+  const [timerInfo, setTimerInfo] = useState('');
+  const [start, setStart] = useState(0);
 
   let buttonClass = 'timer_button';
   if (!pause) {
@@ -15,27 +23,25 @@ const Timer = (itemId) => {
   }
 
   const tick = () => {
-    setCount((state) => state.count + 1);
+    setCount((count) => count + 1);
   };
 
-  const loadTimeToId = () => {
-    localStorage.setItem(JSON.stringify(itemId), JSON.stringify(count));
-  };
-  const toggleTimer = () => {
-    loadTimeToId();
+  const toggleTimer = (e) => {
+    console.log(e.target.id === 'button');
     if (!pause) {
-      setPause((state) => !state.pause);
+      setPause((pause) => !pause);
       const id = setInterval(tick, 1000);
       setTimerId(id);
+      console.log(id);
     } else {
-      setPause((state) => !state.pause);
+      setPause((pause) => !pause);
       clearInterval(timerId);
     }
   };
 
   return (
     <div className="timer">
-      <div className={buttonClass} id="timer-button" onClick={toggleTimer}></div>
+      <button className={buttonClass} id="button" onClick={toggleTimer}></button>
       <span className="timer_info">{formatSeconds(count)}</span>
     </div>
   );
